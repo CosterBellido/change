27306d7f8959fb85cc0f394de6876ab27b104597
coster <coster.bellido@gmail.com>
2021-06-12 21:47:51 -0500

    Mi primer commit

---
+++ primero.js
@@ -0,0 +1,60 @@
+let variable = ["valor", 4, "color", true]
+
+interador.foreach(function(variable) {
+    console.log(iterador)
+})
+
+iterador.forEach(variable => {
+    console.log(variable)
+});
+
+variable.push(34)
+variable.unshift(346)
+
+function dotador() {
+    console.log(2 + 2)
+}
+
+let funcion = function varito() {
+
+}
+
+function memoria(n1, n2) {
+    console.log(n1 + n2)
+}
+
+let resultado= momeria(5, 7)
+
+function variable(a1, a2) {
+    console.log(a1 + a2)
+}
+
+if (caramelo == "baston") {
+    console.log("siempre es posible")
+}
+
+else if(tarro == "cosedor") {
+    console.log("si es lo correcto")
+}
+
+else {
+    console.log(valor)
+}
+
+let i = 0
+while (i < 100) {
+    console.log("color")
+    i ++
+}
+
+let diccionario = {
+    clave: "valor",
+    numero: 3,
+    corto: function varr(a1, a2) {
+        console.log(`Los elementos ${this.clave}`)
+    }
+}
+
+diccionario["corto"](2)
+
+
